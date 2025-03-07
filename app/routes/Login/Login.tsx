b66ca5ffe60ed33b72c5a57de32b0a3d92bb6e4d
Lucas Jaskowiak <lucasjaskowiak@yahoo.fr>
2025-03-07 06:42:39 +0100

    style: mise en page de l'interface login

--- app/routes/Login/Login.tsx
+++ app/routes/Login/Login.tsx
@@ -42,10 +42,32 @@ const Login = () => {
 
   return (
     <div className="form__container">
-      <form method="post" onSubmit={fetchLogin}>
-        <input type="text" name="user_mail" id="mail" placeholder="Entrez votre adresse email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" name="user_password" id="password" placeholder="Entrez votre mot de passe" onChange={(e) => setPassword(e.target.value)} />
-        <input type="submit" value="Se connecter" />
+      <form className="login__form" method="post" onSubmit={fetchLogin}>
+        <div className="login__header__container">
+          <h1 className="login__h1">Alert MNS</h1>
+          <p className="login__p">Connexion à votre plateforme de discussion</p>
+        </div>
+        <div className="login__input__container">
+          <input
+            className="login__input"
+            type="text"
+            name="user_mail"
+            id="mail"
+            placeholder="Entrez votre adresse email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="login__input"
+            type="password"
+            name="user_password"
+            id="password"
+            placeholder="Entrez votre mot de passe"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="login__input__container">
+          <input className="login__input login__submit" type="submit" value="Se connecter" />
+        </div>
       </form>
     </div>
   );
