b12fe8fccc03e19af635fa3253d3364b755a4c36
Lucas <lucasjaskowiak@yahoo.fr>
2025-03-27 16:48:07 +0100

    feat: ajout d'une flèche de déroulement des menus fenêtrés en mobile

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router";
 import { motion } from "motion/react";
+import { useMobileContext } from "~/context/MobileContext";
 
 const Nav: React.FC<{ onClick: (selected: string) => void; activeBtn: string }> = ({ onClick, activeBtn }) => {
+  const { isMobile } = useMobileContext();
+  const [activeArrow, setActiveArrow] = useState(false);
   const handleClick = (selectedBtn: string) => {
     onClick(selectedBtn);
+  };
+
+  const handleActiveArrow = () => {
+    activeArrow ? setActiveArrow(false) : setActiveArrow(true);
   };
 
   return (
@@ -51,47 +58,98 @@ const Nav: React.FC<{ onClick: (selected: string) => void; activeBtn: string }> 
         </motion.button>
       </section>
 
-      <section className="nav__bottom">
-        <motion.button
-          whileHover={{
-            scale: 1.05,
-            transition: { duration: 0.1 },
-          }}
-          whileTap={{ scale: 0.95 }}
-          className={`nav__link ${activeBtn == "agenda" ? "nav__link-active" : ""}`}
-          onClick={() => {
-            handleClick("agenda");
-          }}
-        >
-          <i className="fa-regular fa-calendar"></i>
-        </motion.button>
-        <motion.button
-          whileHover={{
-            scale: 1.05,
-            transition: { duration: 0.1 },
-          }}
-          whileTap={{ scale: 0.95 }}
-          className={`nav__link ${activeBtn == "profile" ? "nav__link-active" : ""}`}
-          onClick={() => {
-            handleClick("profile");
-          }}
-        >
-          <i className="fa-solid fa-user"></i>
-        </motion.button>
-        <motion.button
-          whileHover={{
-            scale: 1.05,
-            transition: { duration: 0.1 },
-          }}
-          whileTap={{ scale: 0.95 }}
-          className={`nav__link ${activeBtn == "settings" ? "nav__link-active" : ""}`}
-          onClick={() => {
-            handleClick("settings");
-          }}
-        >
-          <i className="fa-solid fa-gear"></i>
-        </motion.button>
-      </section>
+      {isMobile ? (
+        <div className="mobile-deploy-button" onClick={handleActiveArrow}>
+          {activeArrow ? <i className="fa-solid fa-angle-down"></i> : <i className="fa-solid fa-angle-up"></i>}
+          {activeArrow ? (
+            <section className="nav__bottom-mobile">
+              <motion.button
+                whileHover={{
+                  scale: 1.05,
+                  transition: { duration: 0.1 },
+                }}
+                whileTap={{ scale: 0.95 }}
+                className={`nav__link nav__bottom-mobile__link ${activeBtn == "agenda" ? "nav__link-active" : ""}`}
+                onClick={() => {
+                  handleClick("agenda");
+                }}
+              >
+                <i className="fa-regular fa-calendar"></i>
+              </motion.button>
+              <motion.button
+                whileHover={{
+                  scale: 1.05,
+                  transition: { duration: 0.1 },
+                }}
+                whileTap={{ scale: 0.95 }}
+                className={`nav__link nav__bottom-mobile__link ${activeBtn == "profile" ? "nav__link-active" : ""}`}
+                onClick={() => {
+                  handleClick("profile");
+                }}
+              >
+                <i className="fa-solid fa-user"></i>
+              </motion.button>
+              <motion.button
+                whileHover={{
+                  scale: 1.05,
+                  transition: { duration: 0.1 },
+                }}
+                whileTap={{ scale: 0.95 }}
+                className={`nav__link nav__bottom-mobile__link ${activeBtn == "settings" ? "nav__link-active" : ""}`}
+                onClick={() => {
+                  handleClick("settings");
+                }}
+              >
+                <i className="fa-solid fa-gear"></i>
+              </motion.button>
+            </section>
+          ) : (
+            ""
+          )}
+        </div>
+      ) : (
+        <section className="nav__bottom">
+          <motion.button
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.1 },
+            }}
+            whileTap={{ scale: 0.95 }}
+            className={`nav__link ${activeBtn == "agenda" ? "nav__link-active" : ""}`}
+            onClick={() => {
+              handleClick("agenda");
+            }}
+          >
+            <i className="fa-regular fa-calendar"></i>
+          </motion.button>
+          <motion.button
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.1 },
+            }}
+            whileTap={{ scale: 0.95 }}
+            className={`nav__link ${activeBtn == "profile" ? "nav__link-active" : ""}`}
+            onClick={() => {
+              handleClick("profile");
+            }}
+          >
+            <i className="fa-solid fa-user"></i>
+          </motion.button>
+          <motion.button
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.1 },
+            }}
+            whileTap={{ scale: 0.95 }}
+            className={`nav__link ${activeBtn == "settings" ? "nav__link-active" : ""}`}
+            onClick={() => {
+              handleClick("settings");
+            }}
+          >
+            <i className="fa-solid fa-gear"></i>
+          </motion.button>
+        </section>
+      )}
     </nav>
   );
 };
